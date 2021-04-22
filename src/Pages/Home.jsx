b45cefb73d9bf685d5e96ee45d0bca69479fda6a
dinhnd96 <dinhnd96@gmail.com>
2021-04-22 18:00:56 +0700

    done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css';
 import logo from '../assets/logo.svg'
 import menubar from '../assets/bars-solid.svg'
 import home from '../assets/home-solid.svg'
-
+import celendar from '../assets/calendar-alt-solid.svg'
 
 function Home() {
   return (
@@ -11,7 +11,7 @@ function Home() {
       <a href="#"><img src={menubar}></img></a>
       <a href="#"><img src={logo}></img></a>
       <a href="#"><img src={home}></img></a>
-      <a href="#">Contact</a>
+      <a href="#"><img src={celendar}></img></a>
     </div>
   );
 }
