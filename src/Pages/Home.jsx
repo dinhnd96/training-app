546673c795a196cede3067fd97a40579639f4d4d
dinhnd96 <dinhnd96@gmail.com>
2021-04-22 08:37:08 +0700

    create loggin

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,31 @@
 function Home() {
-    return (
-      <div>
-        <h2>Home</h2>
+  return (
+    <div className="container">
+      <div className="welcome-screen">
+        <h1>WE LCOME TO</h1>
+        <p>NEW PRODUCT TEAM</p>
+        <span className="welcome-screen-underline"></span>
       </div>
-    );
-  }
+
+      <div className="login-screen">
+        <div className="login-screen-top">
+            <h1>LOGIN</h1>
+            <h1>HERE !</h1>
+            <div className="login-screen-top-logo">
+
+            </div>
+        </div>  
+
+        <div className="login-screen-middle">
+
+        </div>
+
+        <div className="login-screen-bottom">
+
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Home
