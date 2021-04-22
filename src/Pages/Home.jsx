17254c86be3bfdb3e0272be077aa6b2fd989617f
dinhnd96 <dinhnd96@gmail.com>
2021-04-22 18:13:44 +0700

    done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,16 @@
 import './Home.css';
-
-import logo from '../assets/logo.svg'
 import menubar from '../assets/bars-solid.svg'
 import home from '../assets/home-solid.svg'
 import celendar from '../assets/calendar-alt-solid.svg'
+import cogs from '../assets/cogs-solid.svg'
 
 function Home() {
   return (
     <div class="sidenav">
-      <a href="#"><img src={menubar}></img></a>
-      <a href="#"><img src={logo}></img></a>
-      <a href="#"><img src={home}></img></a>
-      <a href="#"><img src={celendar}></img></a>
+      <a className="sidenav-icon" href="#"><img src={menubar}></img></a>
+      <a className="sidenav-icon" href="#"><img src={cogs}></img></a>
+      <a className="sidenav-icon" href="#"><img src={home}></img></a>
+      <a className="sidenav-icon" href="#"><img src={celendar}></img></a>
     </div>
   );
 }
